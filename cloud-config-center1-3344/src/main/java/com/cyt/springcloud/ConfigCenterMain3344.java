a59dafdca2b53d10675d12f412015183062762b9
cyt-cyt <10535337+cyt-cyt@user.noreply.gitee.com>
2023-08-29 16:35:25 +0800

    feat: bus

---
+++ cloud-config-center1-3344/src/main/java/com/cyt/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.cyt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author cyt
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
